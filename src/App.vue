16caa13a87e82fec3e1de13d4ccb8792f07e5b59
banshi9264726 <670039605@qq.com>
2018-06-03 15:13:42 +0800

    显示列表页

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,10 @@
     <div class="container-fluid">
       <div class="row">
         <my-aside></my-aside>
+        <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
+          <!-- 列表 -->
+          <router-view></router-view>
+        </div>
       </div>
     </div>
   </div>
