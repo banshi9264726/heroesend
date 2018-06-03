1a6538a2a7768b02565db069fd381f5af843ce3e
banshi9264726 <670039605@qq.com>
2018-06-03 15:01:39 +0800

    完善侧边栏

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,25 @@
 <template>
   <div id="app">
     <my-header></my-header>
+    <div class="container-fluid">
+      <div class="row">
+        <my-aside></my-aside>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 // 导入头部组件
 import MyHeader from '@/components/myheader'
+// 导入侧边栏组件
+import MyAside from '@/components/myaside'
 export default {
   name: 'App',
   // 注册局部组件
   components: {
-    MyHeader
+    MyHeader,
+    MyAside
   }
 }
 </script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,18 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 
+// 导入组件
+import HeroesList from '@/views/heroes/list'
+import EquipsList from '@/views/equips/list'
+import WeaponsList from '@/views/weapons/list'
+
 Vue.use(Router)
 
 export default new Router({
   routes: [
+    {name: 'home', path: '/', redirect: {name: 'heroes'}},
+    {name: 'heroes', path: '/heroes', component: HeroesList},
+    {name: 'equips', path: '/equips', component: EquipsList},
+    {name: 'weapons', path: '/weapons', component: WeaponsList}
   ]
 })
