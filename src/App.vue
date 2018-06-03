afc6ed41a57bf66af23a04a333d8581bd0ee1fde
banshi9264726 <670039605@qq.com>
2018-06-03 14:49:20 +0800

    分离头部组件

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <my-header></my-header>
+  </div>
+</template>
+
+<script>
+// 导入头部组件
+import MyHeader from '@/components/myheader'
+export default {
+  name: 'App',
+  // 注册局部组件
+  components: {
+    MyHeader
+  }
+}
+</script>
+
+<style>
+</style>
